dacfafd25c563cbfa30fe89ca1e6e4815a8a41d3
Shubhendu Singh <shbhsngh@gmail.com>
2022-05-31 00:32:08 +0530

    Update render.js

--- routes/render.js
+++ routes/render.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET JS */
 router.get('/getJS', function(req, res, next) {
 
-  let scriptCode = `var request=new XMLHttpRequest;request.open("GET","http://178.128.130.194/render/get-image",!0),request.onload=function(){if(request.status>=200&&request.status<400){var e=document.getElementById("some_random_id"),t=document.createElement("img");t.alt="Ad Server Image",t.style="width:400px",t.src=JSON.parse(this.response).data,e.innerHTML="",e.appendChild(t)}else console.log("error")},request.send();` 
+  let scriptCode = `var request=new XMLHttpRequest;request.open("GET","https://warm-wave-66224.herokuapp.com/render/get-image",!0),request.onload=function(){if(request.status>=200&&request.status<400){var e=document.getElementById("some_random_id"),t=document.createElement("img");t.alt="Ad Server Image",t.style="width:400px",t.src=JSON.parse(this.response).data,e.innerHTML="",e.appendChild(t)}else console.log("error")},request.send();` 
 
   res.send(scriptCode);
 });
